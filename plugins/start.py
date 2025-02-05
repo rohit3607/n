6387a157a8317f93a81d8dad6dd28db3de86f490
rohit3607 <rohitplayer87089@gmail.com>
2025-02-05 19:30:23 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -26,9 +26,9 @@ TUT_VID = f"{TUT_VID}"
 @Bot.on_message(filters.command('start') & filters.private & subscribed1 & subscribed2 & subscribed3 & subscribed4)
 async def start_command(client: Client, message: Message):
     id = message.from_user.id
-    if not await present_user(id):
+    if not await db.present_user(id):
         try:
-            await add_user(id)
+            await db.add_user(id)
         except:
             pass
 
