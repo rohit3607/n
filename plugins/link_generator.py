500032d2ce04df33c02980415e2edb192e65a315
rohit3607 <rohitplayer87089@gmail.com>
2025-02-13 11:50:36 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -3,6 +3,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from bot import Bot
+from pyrogram.enums import ParseMode
 from plugins.cbb import *
 from config import *
 from helper_func import encode, get_message_id
