8af7ef794730e15da65f2385e61322ab51c95fb9
rohit3607 <rohitplayer87089@gmail.com>
2025-02-28 00:58:19 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -2,6 +2,6 @@ from pyrogram import filters, Client
 from bot import Bot
 from config import *
 #from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-import imdb, re, asyncio
+#import imdb, re, asyncio
 from database.database import *
 
